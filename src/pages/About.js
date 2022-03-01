98ad560e0f155c5493570e9a9fe7fc78e249ef30
deddypiper <deddypiper@gmail.com>
2022-03-02 00:06:50 +0100

    f

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Loremmxbckjn.odlxnc .jhdbfkxnc,jhx ndk.nsdj.bskjdjns,jdb,d,jnskjn smjb
+        ,nsk,s,djjs.odjsbd.ksjm k.hmndld/lkmnhkemnrni;ukkmrjkendmdm fnbnf
+        .ld./emed j, gpold mark
+       </p>
+    </section>
+  );
+};
+
+export default About;
